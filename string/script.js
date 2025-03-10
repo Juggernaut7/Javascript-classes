be81cebc1e8dd9556c6bab6e47bbb189179e543a
Juggernaut7 <abdulkabir0600@gmail.com>
2025-03-10 05:55:21 -0700

    string

---
+++ string/script.js
@@ -0,0 +1,19 @@
+const mixedStringCase = 'An avalanche of kniowlege that bloom in auntumn'
+const lowerCase = mixedStringCase.toLocaleLowerCase()
+const upperCase = mixedStringCase.toUpperCase()
+console.log(upperCase)
+console.log(lowerCase)
+//indexOf
+// ends with
+// slice
+// splice
+// start withs
+// string index of 
+// lastIndexOf
+// reverse & join
+// repeat 
+// trim
+const cow = 'i love eating cow, it is fucking nutritious'
+
+
+console.log(lastIndexOf(c))
